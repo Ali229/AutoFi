0eda92b240da14e714ae606d7ee4de3ffc8a7237
Ali229 <alinaeem229@hotmail.com>
2016-11-15 12:53:21 -0500

    11/15/2016 Added comments and some code

--- app/src/main/java/com/example/alina/autofi/MainActivity.java
+++ app/src/main/java/com/example/alina/autofi/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.alina.autofi;
-
+//=========================================== Imports ============================================//
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
@@ -8,9 +8,11 @@ import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
-
+//=========================================== Main Class =========================================//
 public class MainActivity extends AppCompatActivity {
+    //=========================================== Declarations ===================================//
     public static final int JOB_ID = 1;
+    //=========================================== On Create ======================================//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,28 +20,31 @@ public class MainActivity extends AppCompatActivity {
         switchFi();
         scheduleJob();
     }
-    @Override protected void onResume() {
+    //=========================================== On Resume ======================================//
+    @Override
+    protected void onResume() {
         super.onResume();
         switchFi();
         scheduleJob();
+        this.finishAffinity();
     }
-    public boolean switchFi(){
+    //=========================================== Switches Off Wifi ==============================//
+    public boolean switchFi() {
         WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         wifi.setWifiEnabled(false);
         return false;
     }
+    //=========================================== Schedule A Job =================================//
     private void scheduleJob() {
         ComponentName serviceName = new ComponentName(this, MyJobService.class);
         JobInfo jobInfo = new JobInfo.Builder(JOB_ID, serviceName)
                 .setMinimumLatency(3600000)
                 .setOverrideDeadline(1800000)
                 .build();
-
         JobScheduler scheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
         int result = scheduler.schedule(jobInfo);
         if (result == JobScheduler.RESULT_SUCCESS) {
-            Toast.makeText(this, "Wifi turned off!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Wifi turned off successfully!", Toast.LENGTH_LONG).show();
         }
     }
-
 }
--- app/src/main/java/com/example/alina/autofi/MyJobService.java
+++ app/src/main/java/com/example/alina/autofi/MyJobService.java
@@ -1,36 +1,29 @@
 package com.example.alina.autofi;
-
+//=========================================== Imports ============================================//
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.widget.Toast;
-
-/**
- * Created by AliNa on 11/15/2016.
- */
-
+//=========================================== Job Class ==========================================//
 public class MyJobService extends JobService {
-
+    //=========================================== Start Job ======================================//
     @Override
     public boolean onStartJob(JobParameters params) {
         // Note: this is preformed on the main thread.
         switchFi();
         return true;
     }
-
-    // Stopping jobs if our job requires change.
-
+    //=========================================== Stop Job =======================================//
     @Override
     public boolean onStopJob(JobParameters params) {
         // Note: return true to reschedule this job.
-        Toast.makeText(this, "Wifi turned back on!", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Wifi turned back on successfully!", Toast.LENGTH_LONG).show();
         return false;
     }
-
-    public void switchFi(){
+    //=========================================== Switches On Wifi ===============================//
+    public void switchFi() {
         WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         wifi.setWifiEnabled(true);
     }
-
 }
