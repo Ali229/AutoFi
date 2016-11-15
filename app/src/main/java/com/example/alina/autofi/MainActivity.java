b9cad84924416fc144a2089077e5d93748acc4a9
Ali229 <alinaeem229@hotmail.com>
2016-11-15 03:13:58 -0500

    Application Developed 11/15/2016

--- app/src/main/java/com/example/alina/autofi/MainActivity.java
+++ app/src/main/java/com/example/alina/autofi/MainActivity.java
@@ -1,20 +1,45 @@
 package com.example.alina.autofi;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int JOB_ID = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        swithFi();
+        switchFi();
+        scheduleJob();
     }
-    public void swithFi(){
+    @Override protected void onResume() {
+        super.onResume();
+        switchFi();
+        scheduleJob();
+    }
+    public boolean switchFi(){
         WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         wifi.setWifiEnabled(false);
+        return false;
     }
+    private void scheduleJob() {
+        ComponentName serviceName = new ComponentName(this, MyJobService.class);
+        JobInfo jobInfo = new JobInfo.Builder(JOB_ID, serviceName)
+                .setMinimumLatency(3600000)
+                .setOverrideDeadline(1800000)
+                .build();
+
+        JobScheduler scheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        int result = scheduler.schedule(jobInfo);
+        if (result == JobScheduler.RESULT_SUCCESS) {
+            Toast.makeText(this, "Wifi turned off!", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
